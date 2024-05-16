e7d0e0915b7dc4e1594f3d6216a213e6b23449ce
nowlingpro <94308062+reed-nowling@users.noreply.github.com>
2024-05-15 21:24:57 -0400

    Combined inductance calculation to torque calculation.

--- Longitudinally Phased Motor/FEMM Files/torque_calculation_script.lua
+++ Longitudinally Phased Motor/FEMM Files/torque_calculation_script.lua
@@ -1,3 +1,20 @@
+-- Function to fetch and compute inductance
+function computeInductance(circuitName)
+    local current, unknown, flux = mo_getcircuitproperties(circuitName)
+    if current and flux then
+        if current ~= 0 then
+            return flux / current
+        else
+            print("Error: Current is zero, cannot compute inductance.")
+            return nil
+        end
+    else
+        print("Error: Coil properties not found or invalid. Check circuit name and analysis results.")
+        return nil
+    end
+end
+
+
 local torque = 0
 local angleStep = -1
 
@@ -5,13 +22,13 @@ mi_analyze()
 mi_loadsolution()
     
 -- Compute the torque using the gap integral method
+mi_selectgroup(9)
+local saliency = computeInductance("Coil")
 mi_clearselected()
-mo_selectblock(0,120)
-mo_selectblock(-104,-62)
-mo_selectblock(104,-62)
+mo_selectblock(0,10)
 mo_zoomnatural()
 torque = mo_blockintegral(22)
-print('0,degrees,' .. torque .. ',Nm')
+print('0,degrees,' .. torque .. ',Nm,' .. saliency .. ',Inductance')
 
 -- Rotate the rotor and calculate torque at each step
 for angle = angleStep, -60, angleStep do  -- adjust the step size as needed
@@ -21,12 +38,11 @@ for angle = angleStep, -60, angleStep do  -- adjust the step size as needed
     mi_analyze()
     mi_loadsolution()
     
-    -- Compute the torque using the gap integral method
+    -- Compute the data
+    saliency = computeInductance("Coil")
     mi_clearselected()
     mo_selectblock(0,10)
-    mo_selectblock(-104,-62)
-    mo_selectblock(104,-62)
     mo_zoomnatural()
     torque = mo_blockintegral(22)
-    print(angle .. ',degrees,' .. torque .. ',Nm')
+    print(angle .. ',degrees,' .. torque .. ',Nm,' .. saliency .. ',Inductance')
 end
